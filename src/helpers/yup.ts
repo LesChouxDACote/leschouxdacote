48def8639d4e424f6b7e69d46ef53f954cd77927
Charles Ilieff <charles.i@rebaze.fr>
2024-09-28 18:35:55 +0200

    yup import

--- src/helpers/yup.ts
+++ src/helpers/yup.ts
@@ -1,4 +1,4 @@
-import { yupResolver } from "@hookform/resolvers/yup/dist/yup"
+import { yupResolver } from "@hookform/resolvers/yup"
 import { USER_ROLE } from "src/constants"
 import { MIN_PASSWORD_LENGTH, validatePassword, validatePhoneNumber, validateSiret } from "src/helpers/validators"
 import { object, string } from "yup"
@@ -7,9 +7,10 @@ const registerSchema = object().shape({
   role: string().required("Champs requis").oneOf([USER_ROLE.BUYER, USER_ROLE.PRODUCER], "Choisir une valeur"),
   siret: string().when("role", {
     is: USER_ROLE.PRODUCER,
-    then: () => string()
-      .required("Champs requis")
-      .test("is-valid-siret", "SIRET invalide", (value) => (value ? validateSiret(value) === true : false)),
+    then: () =>
+      string()
+        .required("Champs requis")
+        .test("is-valid-siret", "SIRET invalide", (value) => (value ? validateSiret(value) === true : false)),
   }),
   name: string().when("role", {
     is: USER_ROLE.PRODUCER,
@@ -21,18 +22,20 @@ const registerSchema = object().shape({
   }),
   description: string().when("role", {
     is: USER_ROLE.PRODUCER,
-    then: () => string()
-      .min(15, "Doit contenir au moins 15 caractères")
-      .max(4000, "Ne doit pas dépasser 4000 caractères")
-      .required("Champs requis"),
+    then: () =>
+      string()
+        .min(15, "Doit contenir au moins 15 caractères")
+        .max(4000, "Ne doit pas dépasser 4000 caractères")
+        .required("Champs requis"),
   }),
   phone: string().when("role", {
     is: USER_ROLE.PRODUCER,
-    then: () => string()
-      .required("Champs requis")
-      .test("is-valid-phone", "Désolé, le numéro de téléphone est invalide", (value) =>
-        value ? validatePhoneNumber(value) === true : false
-      ),
+    then: () =>
+      string()
+        .required("Champs requis")
+        .test("is-valid-phone", "Désolé, le numéro de téléphone est invalide", (value) =>
+          value ? validatePhoneNumber(value) === true : false,
+        ),
   }),
   firstname: string().max(50, "Ne doit pas dépasser 50 caractères").required("Champs requis"),
   lastname: string().max(50, "Ne doit pas dépasser 50 caractères").required("Champs requis"),
@@ -43,7 +46,7 @@ const registerSchema = object().shape({
     .test(
       "is-valid-password",
       `Désolé, le mot de passe doit contenir ${MIN_PASSWORD_LENGTH} caractères minimum dont au moins 1 majuscule, 1 minuscule et 1 chiffre`,
-      (value) => (value ? validatePassword(value) === true : false)
+      (value) => (value ? validatePassword(value) === true : false),
     ),
 })
 
