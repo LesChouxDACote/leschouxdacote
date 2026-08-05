241bffb22c292e6d3dd63ad354b2e76fe2c94879
Automatisation IA <ia@leschouxdacote.fr>
2026-08-05 21:06:01 +0000

    feat: Change le titre pour une blague Capitaliste (Trello #224)

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -2,7 +2,7 @@ import Head from "next/head"
 
 const CONFIG = {
   title: "Les Choux d'à Côté",
-  tagline: "Vente directe : Le circuit plus court des produits alimentaires",
+  tagline: "Le capitalisme, mais en circuit court",
   description:
     "Les Choux d'à Côté - Producteurs, publiez gratuitement des annonces de vos produits. Acheteurs, découvrez les produits locaux près de chez vous.",
   image: process.env.NEXT_PUBLIC_URL + "/opengraph.jpg",
